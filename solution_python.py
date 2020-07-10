fa997ed0f4d32d401d26a4978a4782d121e14607
jingyi2318 <zgjywang@gmail.com>
2020-07-10 14:48:19 -0400

    final submittions

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,49 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.steps = [0]
+        self.version = 0
 
     def add(self, num: int):
-        pass
+        self.value += num
+        if self.version == len(self.steps) - 1: # last value
+            self.steps.append(self.value)
+        else:
+            offset = self.value - self.steps[self.version + 1]
+            self.steps[self.version + 1] = self.value
+            for i in range(self.version + 2, len(self.steps)):
+                self.steps[i] += offset
+        self.version += 1
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        if self.version == len(self.steps) - 1: # last value
+            self.steps.append(self.value)
+        else:
+            offset = self.value - self.steps[self.version + 1]
+            self.steps[self.version + 1] = self.value
+            for i in range(self.version + 2, len(self.steps)):
+                self.steps[i] += offset
+        self.version += 1
 
     def undo(self):
-        pass
+        if self.version > 0:
+            self.version -= 1
+            self.value = self.steps[self.version]
 
     def redo(self):
-        pass
+        if self.version < len(self.steps) - 1:
+            self.version += 1
+            self.value = self.steps[self.version]
 
     def bulk_undo(self, steps: int):
-        pass
+        self.version -= steps
+        if self.version < 0:
+            self.version = 0
+        self.value = self.steps[self.version]
 
     def bulk_redo(self, steps: int):
-        pass
+        self.version += steps
+        if self.version > len(self.steps) - 1:
+            self.version = len(self.steps) - 1
+        self.value = self.steps[self.version]
